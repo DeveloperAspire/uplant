c9ca54c87b2e0491ca9b3a664c12fc281ff47833
DeveloperAspire <franklinokolie86@gmail.com>
2022-06-21 22:52:34 +0100

    Finished the carousel and mobile responsivness

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
+const testimonies = document.querySelectorAll(".testimonial__box");
 
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+testimonies.forEach((testimony, index) => {
+  if (index !== 0) {
+    testimony.classList.add("testimonial__hide");
+  }
+});
+
+let index = 0;
+
+const increment = () => {
+  testimonies[index].classList.add("testimonial__hide");
+
+  if (testimonies.length - 1 === index) {
+    index = 0;
+  } else {
+    index++;
+  }
+
+  testimonies[index].classList.remove("testimonial__hide");
+};
+
+const decrement = () => {
+  testimonies[index].classList.add("testimonial__hide");
+
+  if (index === 0) {
+    index = testimonies.length - 1;
+  } else {
+    index--;
+  }
+
+  testimonies[index].classList.remove("testimonial__hide");
+};
+
+nextBtn.addEventListener("click", () => {
+  increment();
+});
+
+prevBtn.addEventListener("click", () => {
+  decrement();
+});
